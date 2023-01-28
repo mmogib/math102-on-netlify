9c108ef475ccbce56f2256f34e3508fb67a6c536
Mohammed Alshahrani <mmogib@gmail.com>
2023-01-28 12:42:09 +0300

    chapter 5 done

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,38 @@
+<script lang="ts">
+	import { AccordionGroup, AccordionItem } from '@skeletonlabs/skeleton';
+	const chapters_numbers = [5,7,8,9]
+</script>
+
+<svelte:head>
+	<title>MATH102</title>
+</svelte:head>
+
 <!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
 
 <div class="container h-full mx-auto flex justify-center items-center">
 	<div class="space-y-10 text-center">
 		<h2 class="font-bold">Welcome to MATH102 Course.</h2>
-		<a href="ch5.html">Chapter 5</a>
+		<div>
+			{#each chapters_numbers as chap }
+				
+			<AccordionGroup>
+				<!-- Open -->
+				<AccordionItem class="card variant-filled-secondary p-1 mb-2">
+					<svelte:fragment slot="summary">Chapter {chap}</svelte:fragment>
+					<svelte:fragment slot="content">
+						<div class="flex justify-end">
+							<span class="chip p-1">
+								<a class="btn variant-soft-secondary" href="ch{chap}.jl.html" >Go to Page.</a>
+							</span>
+						</div>
+					
+					</svelte:fragment>
+				</AccordionItem>
+				
+			</AccordionGroup>
+			{/each}
+
+		</div>
 	</div>
 </div>
 
